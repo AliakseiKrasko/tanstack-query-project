3dd10c4820ae35d6369e0b5d575b7ddbec36f0a4
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-25 10:55:31 +0300

    feat(api):adde api and interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import {useEffect} from 'react'
 import './App.css'
+import {client} from "./shared/api/client.ts";
 
 function App() {
     useEffect(() => {
         (async function() {
-            const response = await fetch('https://musicfun.it-incubator.app/api/1.0/playlists', {
-                headers: {
-                    'api-key': '0a1c6360-ea6a-430d-a59c-d41a877afc87'
-                }
-            })
-            const data = await response.json()
+            const response = await client.GET('/playlists')
+            const data = response.data
             console.log(data)
         })()
     }, []);
