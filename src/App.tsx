26f735f1612345bab9e2f121ee329260aec676f2
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-26 08:49:33 +0300

    feat: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import './App.css'
 import {useQuery} from "@tanstack/react-query";
 import {client} from "./shared/api/client.ts";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [isVisible, setIsVisible] = useState(true);
 
+    useEffect(() => {
+        const id = setInterval(() => {
+            setIsVisible(prev => !prev);
+        }, 10000)
+        return () => clearInterval(id)
+    }, []);
+    return (
+        <>
+            <h2>Hello IT-INCUBATOR</h2>
+            {isVisible && <PlayLists/>}
+        </>
+    )
+}
+
+const PlayLists = () => {
     const query = useQuery({
+        staleTime: 2000,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
         queryKey: ['playlist'],
         queryFn: () => client.GET('/playlists')
 
     })
-
-  return (
-    <>
-        {query.data?.data?.data.map((item) => (
-            <li>{item.attributes.title}</li>
-        ))}
-    Hello world!
-    </>
-  )
+    return (
+        <div>
+            <ul>
+                {query.data?.data?.data.map((playlist) => (
+                    <li>{playlist.attributes.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App
--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,7 +1,9 @@
 import {createRoot} from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '../style/resen.css'
+import '../style/index.css'
+import App from '../../App.tsx'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 
 const queryClient = new QueryClient()
@@ -9,5 +11,6 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+      <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
 )
