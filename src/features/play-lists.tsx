b4cd82a50b048130609d6da35e43b38098bbb682
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-29 10:06:10 +0300

    feat: add signal and seartch

--- src/features/play-lists.tsx
+++ src/features/play-lists.tsx
@@ -2,16 +2,19 @@ import {keepPreviousData, useQuery} from "@tanstack/react-query";
 
 export const PlayLists = () => {
     const [page, setPage] = useState(1);
+    const [search, setSearch] = useState("");
 
     const query = useQuery({
-        queryKey: ['playlist', page],
-        queryFn: async () => {
+        queryKey: ['playlist', {page, search}],
+        queryFn: async ({ signal }) => {
         const response = await client.GET('/playlists', {
             params: {
                 query: {
                     pageNumber: page,
+                    search
                 }
-            }
+            },
+            signal,
         });
         if (response.error) {
             throw (response as unknown as {error: Error}).error;
@@ -26,6 +29,9 @@ export const PlayLists = () => {
     if (query.isError) return <span>{JSON.stringify(query.error)}</span>
     return (
         <div>
+            <div>
+                <input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search..."  />
+            </div>
             <hr />
             <Pagination
                 pagesCount={query.data.meta.pagesCount}
