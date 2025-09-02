34edab43cfe6fcc81967b48948a186becec397cd
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-09-02 16:21:32 +0300

    feat: added oauth2

--- src/app/layouts/root-layout.tsx
+++ src/app/layouts/root-layout.tsx
@@ -1,14 +1,14 @@
 import {Outlet} from "@tanstack/react-router";
 import Header from "../../shared/ui/header/header.tsx";
 import styles from "./root-layout.module.css";
+import {LoginButton} from "../../features/auth/ui/login-button.tsx";
 
 
-export const RootLayout= () => (
+export const RootLayout = () => (
     <>
-        <Header renderAccountBar={() =><div>Account</div>} />
+        <Header renderAccountBar={() => <LoginButton/>}/>
         <div className={styles.container}>
-             <Outlet/>
+            <Outlet/>
         </div>
-
     </>
 )
--- src/features/auth/ui/login-button.tsx
+++ src/features/auth/ui/login-button.tsx
@@ -2,11 +2,11 @@ import {useMutation} from "@tanstack/react-query";
 import {client} from "../../../shared/api/client.ts";
 
 export const LoginButton = () => {
-    useMutation({
-        mutationFn: async () => {
+    const mutation = useMutation({
+        mutationFn: async ({code}: {code: string}) => {
             const response = await client.POST('/auth/login', {
                 body: {
-                    code: '',
+                    code: code,
                     redirectUri: '',
                     accessTokenTTL: '1d',
                     rememberMe: true
@@ -20,6 +20,8 @@ export const LoginButton = () => {
     })
 
     const handleLoginClick = () => {
+        const callbackUrl = 'http://localhost:5173/oauth/callback'
+        window.open(`https://musicfun.it-incubator.app/api/1.0/auth/oauth-redirect?callbackUrl=${callbackUrl}`, 'apihub-oauth2', 'width=500, height=600');
     }
     return (
         <button onClick={handleLoginClick}>Login with APIHUB</button>
