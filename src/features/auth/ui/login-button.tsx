5c61ecd2fa6d9625cd0e41ef99556e827eefef7f
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-09-02 16:33:31 +0300

    fix: fixed oath2

--- src/features/auth/ui/login-button.tsx
+++ src/features/auth/ui/login-button.tsx
@@ -3,7 +3,7 @@ import {client} from "../../../shared/api/client.ts";
 
 export const LoginButton = () => {
     const mutation = useMutation({
-        mutationFn: async ({code}: {code: string}) => {
+        mutationFn: async ({code}: { code: string }) => {
             const response = await client.POST('/auth/login', {
                 body: {
                     code: code,
@@ -20,10 +20,23 @@ export const LoginButton = () => {
     })
 
     const handleLoginClick = () => {
+        window.addEventListener('message', handOauthMessage)
         const callbackUrl = 'http://localhost:5173/oauth/callback'
         window.open(`https://musicfun.it-incubator.app/api/1.0/auth/oauth-redirect?callbackUrl=${callbackUrl}`, 'apihub-oauth2', 'width=500, height=600');
     }
-    return (
-        <button onClick={handleLoginClick}>Login with APIHUB</button>
-    );
+
+    const handOauthMessage = (event: MessageEvent) => {
+        window.removeEventListener('message', handOauthMessage)
+        if (event.origin !== document.location.origin) {
+            console.log('origin not match')
+            return
+        }
+        const code = event.data.code
+        if (!code) {
+            console.warn('invalid code')
+        }
+        mutation.mutate({code})
+    }
+    return <button onClick={handleLoginClick}>Login with APIHUB</button>
+
 };
